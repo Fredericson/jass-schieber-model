0c90b0173ae94f9b36818112380e789b937bea0a
Fredy Setz <fredy.setz@gmx.ch>
2015-07-26 11:52:53 +0200

    provide missing connectionToServer method

--- src/main/java/ch/jass/model/schieber/api/SchieberServerService.java
+++ src/main/java/ch/jass/model/schieber/api/SchieberServerService.java
@@ -6,12 +6,15 @@ import ch.jass.model.Trumpf;
 
 public interface SchieberServerService {
 
+	public void connectToServer(final SchieberPlayerCallback schieberPlayerCallback);
+
 	public void sendChooseSession(final SessionType sessionType, final String existingSession);
 
 	/**
 	 * Null means geschoben.
 	 * 
 	 * @param trumpf
+	 *            sends the chosen trumpf to the server.
 	 */
 	public void sendChooseTrumpf(final Trumpf trumpf);
 
