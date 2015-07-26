2c217fe47bc7edf34839e746b5563144c9af6e51
Fredy Setz <fredy.setz@gmx.ch>
2015-07-26 11:50:09 +0200

    produce log message in case of card rejected

--- src/main/java/ch/jass/model/schieber/AbstractJassBot.java
+++ src/main/java/ch/jass/model/schieber/AbstractJassBot.java
@@ -1,6 +1,8 @@
 package ch.jass.model.schieber;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ch.jass.model.Card;
 import ch.jass.model.Color;
@@ -10,8 +12,16 @@ import ch.jass.model.schieber.api.SchieberServerService;
 
 public abstract class AbstractJassBot extends SchieberPlayer {
 
+	private final static Logger LOGGER = Logger.getLogger(AbstractJassBot.class.getName());
+
 	public AbstractJassBot(final SchieberServerService schieberService, final String name) {
 		super(schieberService, name);
+	}
+
+	@Override
+	public void requestCardRejected(final Card card) {
+		LOGGER.log(Level.SEVERE, "jassbot " + getName()
+				+ ": I played a wrong card and I don't know what I can do know.");
 	}
 
 	@Override
@@ -30,7 +40,8 @@ public abstract class AbstractJassBot extends SchieberPlayer {
 	 * If the Player is the first in the round the requested Color will be null
 	 * 
 	 * @param requestedColor
-	 * @return Card
+	 *            the color the player has play
+	 * @return Card the chosen Card to play
 	 */
 	public abstract Card chooseCard(Color requestedColor);
 
@@ -41,9 +52,10 @@ public abstract class AbstractJassBot extends SchieberPlayer {
 	}
 
 	/**
-	 * Return null means shifted.
+	 * Return null means geschoben.
 	 * 
 	 * @param geschoben
+	 *            indicate if the team mate did shift the trumpf.
 	 * @return Trumpf
 	 */
 	public abstract Trumpf chooseTrumpf(boolean geschoben);
